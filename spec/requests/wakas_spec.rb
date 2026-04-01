098f4fe6efdec555f12c53f96568d3ac7162d07c
toghisumasu <unpodisale@gmail.com>
2026-04-01 22:34:03 +0900

    Add request specs

--- spec/requests/wakas_spec.rb
+++ spec/requests/wakas_spec.rb
@@ -1,32 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Wakas", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/wakas/index"
+  describe "GET /wakas" do
+    it "一覧ページが表示される" do
+      get wakas_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/wakas/show"
+  describe "GET /wakas/new" do
+    it "新規登録ページが表示される" do
+      get new_waka_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/wakas/new"
+  describe "GET /wakas/:id" do
+    it "詳細ページが表示される" do
+      waka = create(:waka)
+      get waka_path(waka)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/wakas/edit"
-      expect(response).to have_http_status(:success)
+  describe "POST /wakas" do
+    it "有効なデータで和歌を登録できる" do
+      expect {
+        post wakas_path, params: { waka: attributes_for(:waka) }
+      }.to change(Waka, :count).by(1)
+    end
+
+    it "無効なデータでは登録できない" do
+      expect {
+        post wakas_path, params: { waka: attributes_for(:waka, upper_phrase: '') }
+      }.not_to change(Waka, :count)
     end
   end
-
 end
